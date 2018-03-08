4b466651eeaa698030180509265d695641a30004
ireneuszkopta <kopcikxxx@gmail.com>
2018-03-08 12:01:51 +0000

    git config --global --list pong

---
+++ bricksjs.js
@@ -0,0 +1,20 @@
+
+
+
+
+document.addEventListener('keydown',function(e){
+    if(e.keyCode=='13'){
+   msghide.style.display = 'none';
+  init();
+  }},true)
+                  
+
+
+
+
+
+
+
+
+
+
